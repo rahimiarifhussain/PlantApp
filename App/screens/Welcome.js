cd97bb742155d59fe0f030699415062831b3338f
Arif Hussain Rahimi <arifhussain.rahimi84@gmail.com>
2020-02-17 09:56:11 +0430

    ready to navigate

---
+++ App/screens/Welcome.js
@@ -0,0 +1,14 @@
+import {React, Component} from 'react';
+import { View, Text } from 'react-native';
+
+function Welcome() {
+  return (
+    <View>
+      <Text >thi si s</Text>
+
+    </View>
+  );
+} 
+
+  
+export { Welcome};
